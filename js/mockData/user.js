8175abb87e4e470d65f5b80826b39f4ecb617f77
Minh <hoangminh200401@gmailgmail.com>
2023-11-27 22:33:00 +0700

    updatedata

---
+++ js/mockData/user.js
@@ -0,0 +1,90 @@
+export const DataUser = [
+    {
+      imgSrc: "/assets/images/HomePage/pic 1.png",
+      username: "USER 1",
+      rating: 5,
+      review:
+        "Mình đã trải nghiệm qua các dòng hương, đều có hương thơm dịu nhẹ, khử mùi cực đỉnh.",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 2.png",
+      username: "USER 2",
+      rating: 5,
+      review:
+        "Mùi khá thơm nha, dịu nhẹ nhưng nhẹ nhàng nhất vẫn là mùi thanh nhã, rất phù hợp cho các bạn học sinh, sinh viên. Sẽ tiếp tục ủng hộ Refre ^.^",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 3.png",
+      username: "USER 3",
+      rating: 5,
+      review:
+        "Bữa đi siêu thị test được hương chai màu tím hồng á, không biết sao về đặt nhầm hương Mộc Lan nhưng cũng ưng mùi này lắm.",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 4.png",
+      username: "USER 4",
+      rating: 5,
+      review:
+        "Refre màu xanh e thấy xài mùi hơi bị nồng, còn chai xịt màu hồng thì rất oke nhẹ nhàng nhất.",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 5.png",
+      username: "USER 5",
+      rating: 5,
+      review: "Refre đỉnh thiệc nha :v",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 6",
+      rating: 5,
+      review: "Refre đúng kiểu chân ái luông :))",
+    },
+  ];
---
+++ js/services/Homepage.js
@@ -0,0 +1,14 @@
+export const HomePageService = (function($) {
+    
+    function getItems(page, itemsPerPage ,fakeData) {
+      var startIndex = Math.max((page - 1) * itemsPerPage, 0);
+      var endIndex = Math.min(startIndex + itemsPerPage, fakeData.length);
+      console.log(startIndex,endIndex)
+      var displayedItems = fakeData.slice(startIndex, endIndex);
+      return displayedItems;
+    }
+  
+    return {
+      getItems: getItems
+    };
+  })(jQuery);
