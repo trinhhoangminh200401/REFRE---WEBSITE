4143531542c73d893492c3130ac26278d159afbf
Minh <hoangminh200401@gmailgmail.com>
2023-12-01 14:38:17 +0700

    update Ui

---
+++ js/mockData/quiz.js
@@ -0,0 +1,12 @@
+// export const Quiz = [
+//   {
+//     question: "Ba mùi hương yêu thích nhất của bạn là gì",
+//     anwsers: {
+//       [
+//         anwser1: "Hương cây cỏ thực vật",
+//         awnser2: "Hương gỗ, mùi đất",
+//         awnser3: "Hương hoa nhẹ nhàng, tinh tế",
+// ]},
+//     },
+//   },
+// ];
