672e260c73c908f5962d865a9ac2063e73f83907
Minh <hoangminh200401@gmailgmail.com>
2023-12-22 15:00:34 +0700

    update qna

---
+++ js/Qna.js
@@ -0,0 +1,28 @@
+$(document).ready(function (){
+    $(".lazy").Lazy({
+        afterLoad: function (element) {
+          element.addClass("loaded");
+        },
+      });
+      function fadeInOnScroll() {
+        $('.fadein').each(function () {
+           
+            var position = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > position - windowHeight + 100) {
+              
+                $(this).addClass('active');
+            }
+        });
+    }
+    
+    // Call the function on document ready and scroll
+    fadeInOnScroll();
+    $(window).scroll(function () {
+    
+        fadeInOnScroll();
+    });
+         
+});
+
---
+++ js/mockData/QnA.js
@@ -0,0 +1,16 @@
+export const QnA = [
+  {
+    title: "Refre khác biệt gì với các khử mùi trên thị trường",
+    content: "Refre ngoài khả năng giảm tiết mồ hôi ",
+  },
+  {
+    title: "REFRE có phù hợp với da nhạy cảm hay sau khi tẩy lông/cạo không?",
+    content:
+      "Refre với công thức được kiểm nghiệm da liễu dịu nhẹ cho vùng da dưới cánh tay, kể cả da nhạy cảm. Đồng thời, Refre chứa chiết xuất Cam Thảo và Nha Đam làm dịu da ngay sau khi cạo nhỏ ",
+  },
+  {
+    title: "Làm thế nào để chọn mùi hương hợp với bản thân?",
+    content:
+      "Với 9 mùi hương nhẹ nhàng nữ tính tới cá tính quyến rũ, bạn dễ dàng tìm được một mùi hương thể hiện cá tính.",
+  },
+];
