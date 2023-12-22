584ef3976bfd84e9e8cea77026906e71580651ee
Minh <hoangminh200401@gmailgmail.com>
2023-12-22 15:17:01 +0700

    update qna

--- js/About.js
+++ js/About.js
@@ -1,7 +1,42 @@
 $(document).ready(function (){
-    $(".lazy").Lazy({
-        afterLoad: function (element) {
-          element.addClass("loaded");
-        },
+  $(".lazy").Lazy({
+      afterLoad: function (element) {
+        element.addClass("loaded");
+      },
+    });
+    function fadeInOnScroll() {
+      $('.fadein').each(function () {
+         
+          var position = $(this).offset().top;
+          var scroll = $(window).scrollTop();
+          var windowHeight = $(window).height();
+          if (scroll > position - windowHeight + 100) {
+            
+              $(this).addClass('active');
+          }
+          
       });
+  }
+  function rightInScroll(){
+    $('.rightin').each(function () {
+         
+      var position = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > position - windowHeight + 100) {
+        
+          $(this).addClass('active');
+      }
+      
+  });
+  }
+  
+  // Call the function on document ready and scroll
+  fadeInOnScroll();
+  $(window).scroll(function () {
+      rightInScroll()
+      fadeInOnScroll();
+  });
+       
 });
+
--- js/Qna.js
+++ js/Qna.js
@@ -14,6 +14,7 @@ $(document).ready(function (){
               
                 $(this).addClass('active');
             }
+            
         });
     }
     
