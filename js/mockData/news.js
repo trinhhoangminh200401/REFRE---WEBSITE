5c77864ea5fc7ccb3c27dc99001512f26bbde447
Minh <hoangminh200401@gmailgmail.com>
2024-01-22 18:24:34 +0700

    update 22/1

--- js/mockData/news.js
+++ js/mockData/news.js
@@ -1,52 +1,145 @@
-export const NewItems =[{
-    id:1,
-    img : "/assets/images/News/TrangTinTuc/TinTuc_Banner4.png",
-    title :"Trắc nghiệm: Tìm Kiếm sản phẩm khử mùi cơ thể hợp với cá tính bạn",
-    description:"Cùng Refre trắc nghiệm các câu hỏi về sở thích và hoạt động hàng ngày của bạn đã tìm ra giải pháp khử mùi độc nhất",
-    showContent :{
-        URL:"/Quiz.html",
-        contentdiv:``
-    }
-},
-{
-    id:2,
-    img : "/assets/images/News/TrangTinTuc/TinTuc_Banner5.png",
-    title :"Có Refre: Tạm biệt nỗi ám ảnh ố vàng",
-    description:"Bạn mất đi sự tinh tế và sạch sẽ của trang phục chỉ vì ố vàng.Đừng lo, Refre sẽ giúp bạn giải quyết tần tật nỗi niềm này",
-    showContent :{
-        URL:"https://kenh14.vn/uyen-an-tung-anh-moi-cac-thanh-soi-cung-khong-the-phat-hien-ra-diem-dac-biet-nay-20230510230942398.chn",
-        contentdiv:"<div>helle</div>"
-    }
-},
-{
-    id:3,
-    img : "/assets/images/News/TrangTinTuc/TinTuc_Banner4.png",
-    title :"Rạo rực Đón hè, Khóa mùi tỏa  hương, an toàn dịu nhẹ, tự tin vươn cánh cùng refre",
-    description:"Hot news! Tháng 4 này, nóng hơn cả có tiết trời mùa hè chỉ có thể là sự xuất hiện của bộ sưu tập Refre 2023 mới!"
-  ,showContent :{
-    URL:"",
-    contentdiv:"<div>helle</div>"
-}
-},
-{
-    id:4,
-    img : "/assets/images/News/TrangTinTuc/TinTuc_Banner6.png",
-    title :"Rạo rực Đón hè, Khóa mùi tỏa  hương, an toàn dịu nhẹ, tự tin vươn cánh cùng refre",
-    description:"Hot news! Tháng 4 này, nóng hơn cả có tiết trời mùa hè chỉ có thể là sự xuất hiện của bộ sưu tập Refre 2023 mới!"
-    ,showContent :{
-        URL:"",
-        contentdiv:"<div>helle</div>"
-    }
-}
+export const NewItems = [
+  {
+    id: 1,
+    img: "/assets/images/News/TrangTinTuc/TinTuc_Banner4.png",
+    title: "Trắc nghiệm: Tìm Kiếm sản phẩm khử mùi cơ thể hợp với cá tính bạn",
+    description:
+      "Cùng Refre trắc nghiệm các câu hỏi về sở thích và hoạt động hàng ngày của bạn đã tìm ra giải pháp khử mùi độc nhất",
+    showContent: {
+      URL: "/Quiz.html",
+      contentdiv: ``,
+    },
+  },
+  {
+    id: 2,
+    img: "/assets/images/News/TrangTinTuc/TinTuc_Banner5.png",
+    title: "Có Refre: Tạm biệt nỗi ám ảnh ố vàng",
+    description:
+      "Bạn mất đi sự tinh tế và sạch sẽ của trang phục chỉ vì ố vàng.Đừng lo, Refre sẽ giúp bạn giải quyết tần tật nỗi niềm này",
+    showContent: {
+      URL: "",
+      contentdiv: `<div class="w-100" style ="display:flex;justify-content:center;flex-direction:column;">
+      <p class="purpleDark my-3">Bạn từng bao giờ tự hỏi rằng tại sao những chiếc áo trắng mà bạn nâng niu và cất giữ cẩn thận trong tủ lại bị ố vàng ở nách chưa? Nếu câu trả lời là có thì bạn không cô đơn đâu. Đây là một vấn đề phổ biến mà nhiều người trẻ, đặc biệt là học sinh và sinh viên hay những nhân viên văn phòng thường gặp phải với chiếc sơ mi trắng của mình đấy! Việc giữ cho áo quần luôn tươi mới không chỉ là vấn đề về thẩm mỹ mà còn mang ý nghĩa quan trọng trong việc làm tăng sự tự tin của bản thân. 
 
-,{
-    id:10,
-    img:"https://www.refre.com.vn/images/2021/new4.png",
-    title:'NGẮM TRỌN NHỮNG BỨC ẢNH "READY TO BE REAL ME" CỦA NHỮNG CÔ NÀNG "TRENDY" CÙNG REFRE NGẬP TRÀN MẠNG XÃ HỘI',
-    description:"Refre đang được quan tâm và săn đón nồng nhiệt kể từ khi ra mắt bộ sưu tập full sản phẩm ♥️♥️♥️",
-    showContent :{
-        URL:"",
-        contentdiv:`<div class="w-100" style ="display:flex;justify-content:center;flex-direction:column;">
+      </p>
+      <div class="d-flex justify-content-center align-items-center ">
+      <img src =""  alt ="Hình ảnh" />
+      </div>
+        <p class="purpleDark my-3"> Vậy đâu là nguyên nhân khiến áo vùng dưới cánh tay ố vàng? Và có cách nào để giúp bạn tạm biệt nỗi ám ảnh ố vàng trên chiếc áo yêu thích không? </p>
+        <h5 class="h6 fw-bold my-1 purpleDark">Truy tìm nguyên nhân khiến nách áo ố vàng</h5>   
+        <p class="purpleDark my-3">Sự tương tác giữa mồ hôi cơ thể và thành phần chất khử mùi - cụ thể là Aluminum (muối nhôm) - trong sản phẩm chính là nguyên nhân chính dẫn đến tình trạng ố vàng trên áo. Vì muối nhôm cho đến nay được coi là một thành phần rất hiệu quả trong việc ngăn cơ thể tiết mồ hôi nên được sử dụng trong hầu hết các sản phẩm trên thị trường. Tuy nhiên hiện tại đây vẫn là một trong những hoạt chất có nhiều ý kiến trái chiều, tốn nhiều giấy mực của báo chí nhất.</p>
+            <p class="purpleDark my-3"> Bạn có biết, mồ hôi được tiết ra từ các tuyến apocrine tại vùng nách chứa một lượng nhỏ chất béo và protein không? Đây là thành phần tự nhiên của mồ hôi. Song khi tương tác với muối Snhôm có trong lăn khử mùi, qua thời gian, các thành phần này có thể tạo thành mảng bám ố vàng trên áo, làm mất đi tính thẩm mỹ của trang phục. Thế nên, để giải quyết việc ố vàng áo, đơn giản chúng ta chỉ cần đổi sang các sản phẩm khử mùi không chứa muối nhôm. </p>
+            <div class="d-flex justify-content-center align-items-center ">
+            <img src =""  alt ="Hình ảnh" />
+            </div>
+            <h5 class="h6 fw-bold my-1 purpleDark">Refre Mist: Giải pháp không gây ố vàng </h5>   
+        <p class="purpleDark my-2"Refre là thương hiệu đến từ công ty Rohto–Mentholatum, chuyên về các dòng lăn khử mùi và xịt khử mùi cơ thể. Với khả năng khử mùi và ngăn tiết mồ hôi hiệu quả đến 48 giờ, giá thành hợp lý, Refre đã trở thành một thương hiệu quen thuộc của đông đảo giới trẻ.  </p>
+        <p class="purpleDark my-2">Các hạt phấn cực mịn chiết xuất từ Cây Tre có trong các sản phẩm khử mùi của Refre giúp hút ẩm liên tục, giữ cho vùng da dưới cánh tay luôn mềm mịn, khô thoáng. Đáng nói, Refre có dòng xịt khử mùi với công thức độc đáo không chứa cồn, Paraben và muối nhôm vừa dịu nhẹ, không gây kích ứng da, vừa không gây ố vàng trên áo. </p>
+        <p class="purpleDark my-2">Hiện Refre có 2 dòng sản phẩm là Refre Whitening Perfume với 5 mùi hương nước hoa sang trọng như Deluxe – Hương sang trọng, Sweetie – Hương ngọt ngào, Sophis – Hương thanh nhã, Baby Powder – Hương phấn thơm hay In Love – Hương tình yêu và Refre Natural, nổi bật với công thức có nhiều thành phần chiết xuất tự nhiên và mùi hương đậm chất cây cỏ, thực vật như hương của trà xanh - Green Tea, hương thảo - Rosemary, hồng mộc lan - Rosa Magnolia, cho đến hương gỗ - Woody. </p>
+        <p class="purpleDark my-2">Refre xịt khử mùi là một sản phẩm “đa - Z - năng” vì bạn có thể sử dụng nó ở bất kì vùng cơ thể nào “có rau mùi” như nách, chân,... hoặc kể cả khi bạn vừa “enjoy” một buổi tiệc thịt nướng thì công nghệ khóa mùi thông minh từ Refre cũng sẽ giúp bạn “bắt trọn” những phân tử mùi khó chịu lại một cách tức thì, để bạn không làm phiền chiếc mũi của những người xung quanh!</p>
+
+        <div class="purpleDark my-2"><span class="fw-bold " style="margin-right:0.5em">Lưu ý:</span>Tránh tiếp xúc với mắt và vùng da bị tổn thương. Không sử dụng lên da bị kích ứng. Nếu có bất kỳ dấu hiệu phản ứng không mong muốn, ngừng sử dụng ngay và tham khảo ý kiến ​​bác sĩ.</div>
+
+        <p class="purpleDark my-2">Tạm biệt nỗi ám ảnh ố vàng, hãy để Refre trở thành người bạn đồng hành đáng tin cậy của bạn trên chặng đường khử mùi cơ thể và bảo vệ áo quần tươi mới sạch sẽ. Cùng trải nghiệm sự khác biệt mà Refre mang lại ngay tại Shopee, Lazada chính hãng hoặc hệ thống Hasaki trên toàn quốc. </p>
+
+            </div>`,
+    },
+  },
+  {
+    id: 3,
+    img: "/assets/images/News/TrangTinTuc/AZ.jpg",
+    title:
+      "Từ A-Z: Sử dụng sản phẩm khử mùi đúng cách để phát huy tối đa hiệu quả",
+    description:
+      "“Rau mùi” là chuyện nhỏ nếu như bạn biết cách sử dụng khử mùi đúng cách. Cùng Refre tìm hiểu xem bạn đã tận dụng hết “công lực” các sản phẩm khử mùi chưa nhé.",
+    showContent: {
+      URL: "",
+      contentdiv: `<div class="w-100" style ="display:flex;justify-content:center;flex-direction:column;">
+    <p class="purpleDark my-3">Tình trạng "rau mùi" từ cơ thể hẳn là một trong những vấn đề nhức nhối của không ít các bạn trẻ. Điều này không chỉ gây khó chịu mà còn ảnh hưởng đến sự tự tin khi giao tiếp của ta. Đó là lý do tại sao việc sử dụng sản phẩm khử mùi đúng cách rất quan trọng. Trong bài viết này, Refre sẽ hướng dẫn bạn từ A đến Z cách sử dụng các sản phẩm khử mùi “chuẩn chỉnh” để bạn có thể tận dụng tối đa hiệu quả của chúng.
+    </p>
+    <div class="d-flex justify-content-center align-items-center ">
+    <img src ="/assets/images/News/TrangTinTuc/azimg.jpg" width="500" alt ="Hình ảnh" />
+    </div>
+    <h5 class="h6 fw-bold my-3 purpleDark">Hiểu về nhu cầu và mục đích sử dụng</h5>   
+     <p class="purpleDark my-3"> 
+     Trước tiên, bạn cần chọn được một loại kh	ử mùi phù hợp với nhu cầu. Chính vì vậy mà việc hiểu rõ về nhu cầu và mục đích sử dụng của mình cũng là việc đầu tiên cần được xác định!
+     </p>
+     <p class="purpleDark my-3">Bạn muốn một sản phẩm vừa có thể khử mùi, lại vừa có hương thơm thơm ngát lưu lại thật lâu? Hay bạn cần một sản phẩm dịu nhẹ, không gây kích ứng da bởi bạn sở hữu làn da cực kỳ nhạy cảm? Hãy xác định vấn đề về mùi cơ thể mà bạn muốn khử và tìm hiểu về các loại sản phẩm khử mùi phổ biến. 
+     </p>
+     <div class="d-flex justify-content-center align-items-center">
+     <img src ="" alt ="Hình ảnh" />
+     </div>
+     <p class="purpleDark my-3"> Trên thị trường hiện nay có nhiều dạng sản phẩm khử mùi cơ thể khác nhau. Song phổ biến nhất có thể kể đến là dạng lăn và xịt khử mùi. Lăn khử mùi có dạng ống lăn, nhỏ, vừa tay, thường được dùng cho vùng da dưới cánh tay, nhưng chỉ có thể sử dụng ở nhà, không quá tiện lợi. Trong khi đó, xịt khử mùi lại có dạng vòi tia xịt, và có tính tiện dụng hơn, vừa ngăn mùi hôi, vừa tạo mùi thơm nhẹ nhàng cho cơ thể khi xịt vào cổ, cổ tay…
+     </p>
+     <p class ="purpleDark my-3
+     ">Với khả năng khử mùi và ngăn tiết mồ hôi hiệu quả đến 48 giờ, giá thành hợp lý, Refre đã trở thành một thương hiệu quen thuộc của đông đảo giới trẻ. Các hạt phấn cực mịn chiết xuất từ Cây Tre có trong các sản phẩm khử mùi của Refre giúp hút ẩm liên tục, giữ cho vùng da dưới cánh tay luôn mềm mịn, khô thoáng. Đáng nói, tất cả các sản phẩm của Refre đều không chứa cồn và Paraben, cực kỳ dịu nhẹ, không gây kích ứng cho da, và không gây ố vàng trên áo. Hai dạng lăn và xịt cùng 9 mùi hương khác nhau tha hồ cho bạn lựa chọn thể hiện cá tính. </p>
+    <div class="contentImage "> 
+    <div class="d-flex justify-content-center align-items-center ">
+    <img src ="" alt ="Hình ảnh" />
+    </div>
+    <div class="image-description text-center">
+       <p class="fst-italic purpleDark"> Dòng Refre Whitening Perfume với 5 mùi hương nước hoa sang trọng </p>
+       <p class="fst-italic purpleDark">Dòng Refre Natural nổi bật với công thức có nhiều thành phần chiết xuất tự nhiên và mùi hương đậm chất cây cỏ, thực vật </p>
+     </div>
+    </div>
+    <h5 class="h6 fw-bold my-3 purpleDark">Cách sử dụng ĐÚNG!</h5>   
+    <p class="purpleDark my-3"> Bất cứ sản phẩm nào cũng đều cần được sử dụng đúng cách thì mới phát huy được hiệu quả tối đa. Để Refre nhắc bạn mẹo triệt để “khai phá tiềm năng” của chai lăn/xịt khử mùi của bạn nhé!
+
+    </p>
+    <ul
+    class="purpleDark">
+    Đơn giản lắm, chỉ cần tuân thủ quy trình áp dụng sau đây:   
+    <li>Bước 1: Không gì khác ngoài làm sạch. Thời điểm lý tưởng nhất để sử dụng khử mùi chính là ngay sau khi tắm.
+    </li>
+    <li> Bước 2: Hãy đảm bảo vùng da khô hoàn toàn trước khi áp dụng sản phẩm vì điều này giúp sản phẩm thẩm thấu vào da tốt hơn và đảm bảo hiệu quả khử mùi kéo dài.
+    </li>
+    <li>Bước 3: Làm theo hướng dẫn trên bao bì sản phẩm, lắc đều sản phẩm trước khi sử dụng và lăn/xịt vào vùng da dưới cánh tay. 
+    </li>
+    </ul>
+    <div class="d-flex justify-content-center align-items-center ">
+    <img src ="" alt ="Hình ảnh" />
+    </div>
+   
+    <h5 class="h6 fw-bold my-3 purpleDark">Lưu ý</h5>   
+    <ul
+    class="purpleDark">
+    Bên cạnh đó, nhằm đảm bảo an toàn và hiệu quả khi sử dụng sản phẩm khử mùi, bạn cần ghi nhớ những lưu ý quan trọng sau: 
+
+    <li>Tránh tiếp xúc với các vùng nhạy cảm trên cơ thể.
+    </li>
+    <li> Kiểm tra phản ứng kích ứng/dị ứng trước khi sử dụng.    </li>
+    <li> Bảo quản đúng cách để sản phẩm không bị hỏng.
+    </li>
+    <li>Để duy trì hiệu quả khử mùi, bạn có thể mang theo xịt khử mùi để tái áp dụng trong quá trình hoạt động hàng ngày. </li>
+    </ul>
+    <p class="purpleDark">Với quy trình áp dụng trên, Refre tin chắc rằng bạn sẽ có thể gạt bỏ nỗi lo “rau mùi” và tự tin tận hưởng cảm giác thoải mái suốt 24 giờ. Cùng khám phá ngay các dòng sản phẩm lăn và xịt khử mùi “xịn xò” Refre ngay tại Shopee, Lazada chính hãng hoặc hệ thống Hasaki trên toàn quốc. 
+    </p>
+   </div>`,
+    },
+  },
+  {
+    id: 4,
+    img: "/assets/images/News/TrangTinTuc/TinTuc_Banner1.png",
+    title:
+      "Rạo rực Đón hè, Khóa mùi tỏa  hương, an toàn dịu nhẹ, tự tin vươn cánh cùng refre",
+    description:
+      "Hot news! Tháng 4 này, nóng hơn cả có tiết trời mùa hè chỉ có thể là sự xuất hiện của bộ sưu tập Refre 2023 mới!",
+    showContent: {
+      URL: "https://kenh14.vn/uyen-an-tung-anh-moi-cac-thanh-soi-cung-khong-the-phat-hien-ra-diem-dac-biet-nay-20230510230942398.chn",
+      contentdiv: "",
+    },
+  },
+
+  {
+    id: 10,
+    img: "https://www.refre.com.vn/images/2021/new4.png",
+    title:
+      'NGẮM TRỌN NHỮNG BỨC ẢNH "READY TO BE REAL ME" CỦA NHỮNG CÔ NÀNG "TRENDY" CÙNG REFRE NGẬP TRÀN MẠNG XÃ HỘI',
+    description:
+      "Refre đang được quan tâm và săn đón nồng nhiệt kể từ khi ra mắt bộ sưu tập full sản phẩm ♥️♥️♥️",
+    showContent: {
+      URL: "",
+      contentdiv: `<div class="w-100" style ="display:flex;justify-content:center;flex-direction:column;">
          <p class="purpleDark">Ngoài ra, Refre cũng có một thông tin cực quan trọng muốn thông báo với nàng. Refre sẽ sớm gửi đến tay nàng phiên bản "LIMITED" này.</p>
          <p class="purpleDark"> Are you ready?</p>
 
@@ -55,7 +148,7 @@ export const NewItems =[{
          <img src="https://www.refre.com.vn/images/2021/News/picture7.jpg" style="width:80% ;margin-top: 2em"  />
           <p class=""> Chúng ta cùng điểm sơ qua một số hình ảnh "READY GIRLS" của các cô nàng xinh đẹp ngập tràn mạng xã hội đang "làm mưa làm gió" thời gian gần đây.
             </p>
-        <p class="">Refre khiến các nàng mê mẩn, nối tay nhau bắt "trend" siêu yêu. Nàng muốn năng động như cô nàng Võ Ngọc Trân, Phi Linh hay nhẹ nhàng như cô nàng Minh Tuyền và Mai Hà Hoàng Yến nào.
+        <p class="purpleDark">Refre khiến các nàng mê mẩn, nối tay nhau bắt "trend" siêu yêu. Nàng muốn năng động như cô nàng Võ Ngọc Trân, Phi Linh hay nhẹ nhàng như cô nàng Minh Tuyền và Mai Hà Hoàng Yến nào.
 
         </p>
          </div >
@@ -66,7 +159,7 @@ export const NewItems =[{
          <img src="https://www.refre.com.vn/images/2021/News/picture12.jpg "  style="width:80%;margin-top: 2em" />
         
          </div>
-        </div>`
-    }
-}
-]
+        </div>`,
+    },
+  },
+];
