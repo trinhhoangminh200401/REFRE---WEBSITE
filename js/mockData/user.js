af286c7c383a57b17681e68616e9f29b7276d7e7
Minh <hoangminh200401@gmailgmail.com>
2024-02-23 11:17:51 +0700

    ui/user

--- js/mockData/user.js
+++ js/mockData/user.js
@@ -4,7 +4,7 @@ export const DataUser = [
       username: "USER 1",
       rating: 5,
       review:
-        "Mình đã trải nghiệm qua các dòng hương, đều có hương thơm dịu nhẹ, khử mùi cực đỉnh.",
+        "Mình đã trải nghiệm qua các dòng hương, đều có hương thơm dịu nhẹ, khử mùi cực đỉnh <3",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 2.png",
@@ -18,68 +18,56 @@ export const DataUser = [
       username: "USER 3",
       rating: 5,
       review:
-        "Bữa đi siêu thị test được hương chai màu tím hồng á, không biết sao về đặt nhầm hương Mộc Lan nhưng cũng ưng mùi này lắm.",
+        "Bao bì Refre đợt này siêu xinh, mùi giữ được lâu. dùng thích lắm luôn. Giá thành rẻ, phù hợp với mọi nhà.",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 4.png",
       username: "USER 4",
       rating: 5,
       review:
-        "Refre màu xanh e thấy xài mùi hơi bị nồng, còn chai xịt màu hồng thì rất oke nhẹ nhàng nhất.",
+        "Chai màu hồng hình như Rose Magnolia, mùi thơm cực đỉnh luôn í",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 5.png",
       username: "USER 5",
       rating: 5,
-      review: "Refre đỉnh thiệc nha :v",
+      review: "Đang sài thử chai tím Deluxe nhưng mà mình thấy chai xanh lá thơm nhất.",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 6",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
-    },
-    {
-      imgSrc: "/assets/images/HomePage/pic 6.png",
-      username: "USER 6",
-      rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
-    },
-    {
-      imgSrc: "/assets/images/HomePage/pic 6.png",
-      username: "USER 6",
-      rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "Bữa đi siêu thị test được hương chai màu tím hồng á, không biết sao về đặt nhầm hương Mộc Lan nhưng cũng ưng mùi này lắm",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 7",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "Hôi nách không lo, vì tui toàn dùng lăn khử mùi bên đầy thôi á chèn.",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 8",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "Với tui mùi màu “xanh lá” là Ontop. Góp ý thêm Refre nên mở Tiktok shop cho tui mua dễ hơn i ạ",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 9",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "tiện dùng, sài cũng gần 4 năm rồi, chỉ trung thành duy nhất màu tím mộng mơ.",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 10",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "Refre thơm lâu. Mùi dễ chịu.Mình hay dùng xịt quần áo",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
       username: "USER 11",
       rating: 5,
-      review: "Refre đúng kiểu chân ái luông :))",
+      review: "Cái Refre mình dùng rồi nha. Nó ngăn mồ hôi cực tốt, xài cả ngày mà nách vẫn khô ráo, mùi thơm xỉĩu và không ố vàng áo.",
     },
     {
       imgSrc: "/assets/images/HomePage/pic 6.png",
@@ -87,4 +75,54 @@ export const DataUser = [
       rating: 5,
       review: "Refre đúng kiểu chân ái luông :))",
     },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 13",
+      rating: 5,
+      review: "Refre thơm lâu. Mùi dễ chịu.Mình hay dùng xịt quần áo",
+    },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 14",
+      rating: 5,
+      review: "Mình dùng thấy ok nè, không bị thâm mà giữ mùi lâu",
+  },
+    {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 15",
+      rating: 5,
+      review: "Trung thành Refre dạng xịt chắc cũng mấy năm rồi. 1 tình eo ko thay đổi trường tồn theo năm tháng. Xịt phát khô ngày :')  ko bết thơm cả ngày ",
+  },
+        {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 16",
+      rating: 5,
+      review: "Refre màu xanh e xài thấy mùi hơi bị nồng, còn chai xị màu hồng thì rất oke nhẹ nhàng nhất ",
+  },
+          {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 17",
+      rating: 5,
+      review: "Refre đỉnh thiệc nha :v",
+  },
+              {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 18",
+      rating: 5,
+      review: "Refre tui sài giữ mùi lâu 2 ngày vẫn còn thơm, càng vận động càng thơm í. ",
+  },
+    
+              {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 19",
+      rating: 5,
+      review: "Refre dùng ok lắm kiềm mồ hôi rất tốt nha nên dùng loại chai xịt hơn nha",
+  },          
+              
+              {
+      imgSrc: "/assets/images/HomePage/pic 6.png",
+      username: "USER 20",
+      rating: 5,
+      review: "Refre hồng nhạt tuyệt vời luôn, vừa lăn xong ai đứng gần cũng hỏi xịt nước hoa hả, giữ mùi cả ngày luôn nhé! ",
+  },
   ];
